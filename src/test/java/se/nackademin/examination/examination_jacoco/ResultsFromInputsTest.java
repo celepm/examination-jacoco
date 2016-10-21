14ef759ff1fcaaea71e7edd3a1e54f3ee280e9a7
Mustafa Celep <mcelep@gmail.com>
2016-10-21 09:33:53 +0200

    Added ResultFromInputsTest

--- src/test/java/se/nackademin/examination/examination_jacoco/ResultsFromInputsTest.java
+++ src/test/java/se/nackademin/examination/examination_jacoco/ResultsFromInputsTest.java
@@ -5,12 +5,29 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class ResultsFromInputsTest {
-
+	ResultFromInputs resultFromInputs = new ResultFromInputs();
+	
 	@Test
-	public void testAgeGettersAndSettersMethods() {
-		ResultFromInputs resultsFromInputs = new ResultFromInputs();
-		resultsFromInputs.setResultForNameLength(10);
-		assertEquals("The result should be 10", resultsFromInputs.getResultForNameLength(), 10);
+	public void testResultForNameLength() {
+		resultFromInputs.setResultForNameLength(1);
+		assertEquals("The result should be 1", resultFromInputs.getResultForNameLength(), 1);
 	}
-
+	
+	@Test
+	public void testResultForGender() {
+		resultFromInputs.setResultForGender(0);
+		assertEquals("The result should be 0", resultFromInputs.getResultForGender(), 0);
+	}
+	
+	@Test
+	public void testResultForHomeCity() {
+		resultFromInputs.setResultForHomeCity(5);
+		assertEquals("The result should be 5", resultFromInputs.getResultForHomeCity(), 5);
+	}
+	
+	@Test
+	public void testResultForAge() {
+		resultFromInputs.setResultForAge(1);
+		assertEquals("The result should be 1", resultFromInputs.getResultForAge(), 1);
+	}
 }
